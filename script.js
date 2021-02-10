435d32c66ef9fb523cc792d44c33faf17437db2c
ajarek <ajarek@poczta.onet.pl>
2021-02-10 22:34:31 +0100

    2 commit

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const drzwi2 = document.querySelector('.drzwi2')
 const display = document.querySelector('.display')
 const btn = document.querySelectorAll('.btn')
 
-
 let tablica=[0]
 
 
@@ -13,10 +12,12 @@ otworz.addEventListener('click', () => {
     drzwi1.style.width = '0'
     drzwi2.style.width = '0'
 })
+
 zamknij.addEventListener('click', () => {
     drzwi1.style.width = `154px`
     drzwi2.style.width = `154px`
 })
+
 btn.forEach((element) => {
     element.addEventListener('click', () => {
         
@@ -25,16 +26,17 @@ btn.forEach((element) => {
         for(i=0;i<tablica.length;i++){
             if(tablica[tablica.length-1]>tablica[tablica.length-2]){
                
-                animateUp(display, parseInt(tablica[tablica.length-2]), parseInt(tablica[tablica.length-1]), 6000);
+                animateUp(display, parseInt(tablica[tablica.length-2]), parseInt(tablica[tablica.length-1]), 5000);
             }
             if(tablica[tablica.length-1]<tablica[tablica.length-2]){
               
-                animateDown(display, parseInt(tablica[tablica.length-2]), parseInt(tablica[tablica.length-1]), 6000);
+                animateDown(display, parseInt(tablica[tablica.length-2]), parseInt(tablica[tablica.length-1]), 5000);
             }
         }
     })
 
 })
+
 function animateUp(display, start, end, duration) {
     let startTimestamp = null;
     const step = (timestamp) => {
@@ -44,9 +46,10 @@ function animateUp(display, start, end, duration) {
       if (progress < 1) {
         window.requestAnimationFrame(step);
       }
-    };
+    }
     window.requestAnimationFrame(step);
   }
+
   function animateDown(display, start, end, duration) {
     let startTimestamp = null;
     const step = (timestamp) => {
